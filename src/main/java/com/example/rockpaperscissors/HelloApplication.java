b81bada944929ce59587dee67d49dec35cedc650
Sebastian R. <Sebastijan97@web.de>
2025-09-08 14:00:28 +0200

    Added CSS

--- src/main/java/com/example/rockpaperscissors/HelloApplication.java
+++ src/main/java/com/example/rockpaperscissors/HelloApplication.java
@@ -1,17 +1,18 @@
 package com.example.rockpaperscissors;
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1280);
+        scene.getStylesheets().add(
+            HelloApplication.class.getResource("style.css").toExternalForm());
         stage.setTitle("Rock Paper Scissors by Sebastian Rusch");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/rockpaperscissors/HelloController.java
+++ src/main/java/com/example/rockpaperscissors/HelloController.java
@@ -1,6 +1,4 @@
 package com.example.rockpaperscissors;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
